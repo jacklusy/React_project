bc6ab60fe36a82e3b84385a201de4fb70c5e292e
unknown <salwayehyaabuafash@gmail.com>
2023-02-28 13:21:00 +0200

    edit style

--- frontend/src/components/body/LeftSidebar.js
+++ frontend/src/components/body/LeftSidebar.js
@@ -18,8 +18,8 @@ const LeftSidebar = () => {
               <ul id="iq-sidebar-toggle" className="iq-menu">
 
                 <li className="active">
-                  <a href="/EditProfile/:id/edit" className=" ">
-                    <i className="las la-user" /><span>Edit Profile</span>
+                  <a href="/Profile" className=" ">
+                    <i className="las la-user" /><span>Profile</span>
                   </a>
                 </li>
                
@@ -28,10 +28,7 @@ const LeftSidebar = () => {
                     <i className="las la-users" /><span>Group</span>
                   </a>
                 </li>
-                
-                <li className=" ">
-                  <a href="/ProfileForum" ><i className="ri-focus-2-line" /><span>Profile Forum</span><i className="ri-arrow-right-s-line iq-arrow-right" /></a>
-                </li>
+          
 
                 <li className="">
                   <a href="/Chat" ><i className="ri-pages-line" /><span>Friends</span><i className="ri-arrow-right-s-line iq-arrow-right" /></a>
--- frontend/src/components/body/Navbar.js
+++ frontend/src/components/body/Navbar.js
@@ -31,11 +31,7 @@ function Navbar() {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav  ms-auto navbar-list">
-              <li>
-                <a href="/" className="  d-flex align-items-center">
-                  <i className="ri-home-line" />
-                </a>
-              </li>
+             
               <li className="nav-item dropdown">
                 <a href="#" className="dropdown-toggle" id="group-drop" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i className="ri-group-line" /></a>
                 <div className="sub-drop sub-drop-large dropdown-menu" aria-labelledby="group-drop">
@@ -114,159 +110,14 @@ function Navbar() {
                   </div>
                 </div>
               </li>
-              <li className="nav-item dropdown">
-                <a href="#" className="search-toggle   dropdown-toggle" id="notification-drop" data-bs-toggle="dropdown">
-                  <i className="ri-notification-4-line" />
-                </a>
-                <div className="sub-drop dropdown-menu" aria-labelledby="notification-drop">
-                  <div className="card shadow-none m-0">
-                    <div className="card-header d-flex justify-content-between bg-primary">
-                      <div className="header-title bg-primary">
-                        <h5 className="mb-0 text-white">All Notifications</h5>
-                      </div>
-                      <small className="badge  bg-light text-dark">4</small>
-                    </div>
-                    <div className="card-body p-0">
-                      <a href="#" className="iq-sub-card">
-                        <div className="d-flex align-items-center">
-                          <div className>
-                            <img className="avatar-40 rounded" src="/images/user/01.jpg" alt="" />
-                          </div>
-                          <div className="ms-3 w-100">
-                            <h6 className="mb-0 ">Emma Watson Bni</h6>
-                            <div className="d-flex justify-content-between align-items-center">
-                              <p className="mb-0">95 MB</p>
-                              <small className="float-right font-size-12">Just Now</small>
-                            </div>
-                          </div>
-                        </div>
-                      </a>
-                      <a href="#" className="iq-sub-card">
-                        <div className="d-flex align-items-center">
-                          <div className>
-                            <img className="avatar-40 rounded" src="/images/user/02.jpg" alt="" />
-                          </div>
-                          <div className="ms-3 w-100">
-                            <h6 className="mb-0 ">New customer is join</h6>
-                            <div className="d-flex justify-content-between align-items-center">
-                              <p className="mb-0">Cyst Bni</p>
-                              <small className="float-right font-size-12">5 days ago</small>
-                            </div>
-                          </div>
-                        </div>
-                      </a>
-                      <a href="#" className="iq-sub-card">
-                        <div className="d-flex align-items-center">
-                          <div className>
-                            <img className="avatar-40 rounded" src="/images/user/03.jpg" alt="" />
-                          </div>
-                          <div className="ms-3 w-100">
-                            <h6 className="mb-0 ">Two customer is left</h6>
-                            <div className="d-flex justify-content-between align-items-center">
-                              <p className="mb-0">Cyst Bni</p>
-                              <small className="float-right font-size-12">2 days ago</small>
-                            </div>  
-                          </div>
-                        </div>
-                      </a>
-                      <a href="#" className="iq-sub-card">
-                        <div className="d-flex align-items-center">
-                          <div className>
-                            <img className="avatar-40 rounded" src="/images/user/04.jpg" alt="" />
-                          </div>
-                          <div className="w-100 ms-3">
-                            <h6 className="mb-0 ">New Mail from Fenny</h6>
-                            <div className="d-flex justify-content-between align-items-center">
-                              <p className="mb-0">Cyst Bni</p>
-                              <small className="float-right font-size-12">3 days ago</small>
-                            </div>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li className="nav-item dropdown">
-                <a href="/Email" className="dropdown-toggle" id="mail-drop"  aria-haspopup="true" aria-expanded="false">
-                  <i className="ri-mail-line" />
-                </a>
-                <div className="sub-drop dropdown-menu" aria-labelledby="mail-drop">
-                  <div className="card shadow-none m-0">
-                    <div className="card-header d-flex justify-content-between bg-primary">
-                      <div className="header-title bg-primary">
-                        <h5 className="mb-0 text-white">All Message</h5>
-                      </div>
-                      <small className="badge bg-light text-dark">4</small>
-                    </div>
-                    <div className="card-body p-0 ">
-                      <a href="#" className="iq-sub-card">
-                        <div className="d-flex  align-items-center">
-                          <div className>
-                            <img className="avatar-40 rounded" src="/images/user/01.jpg" alt="" />
-                          </div>
-                          <div className=" w-100 ms-3">
-                            <h6 className="mb-0 ">Bni Emma Watson</h6>
-                            <small className="float-left font-size-12">13 Jun</small>
-                          </div>
-                        </div>
-                      </a>
-                      <a href="#" className="iq-sub-card">
-                        <div className="d-flex align-items-center">
-                          <div className>
-                            <img className="avatar-40 rounded" src="/images/user/02.jpg" alt="" />
-                          </div>
-                          <div className="ms-3">
-                            <h6 className="mb-0 ">Lorem Ipsum Watson</h6>
-                            <small className="float-left font-size-12">20 Apr</small>
-                          </div>
-                        </div>
-                      </a>
-                      <a href="#" className="iq-sub-card">
-                        <div className="d-flex align-items-center">
-                          <div className>
-                            <img className="avatar-40 rounded" src="/images/user/03.jpg" alt="" />
-                          </div>
-                          <div className="ms-3">
-                            <h6 className="mb-0 ">Why do we use it?</h6>
-                            <small className="float-left font-size-12">30 Jun</small>
-                          </div>
-                        </div>
-                      </a>
-                      <a href="#" className="iq-sub-card">
-                        <div className="d-flex align-items-center">
-                          <div className>
-                            <img className="avatar-40 rounded" src="/images/user/04.jpg" alt="" />
-                          </div>
-                          <div className="ms-3">
-                            <h6 className="mb-0 ">Variations Passages</h6>
-                            <small className="float-left font-size-12">12 Sep</small>
-                          </div>
-                        </div>
-                      </a>
-                      <a href="#" className="iq-sub-card">
-                        <div className="d-flex align-items-center">
-                          <div className>
-                            <img className="avatar-40 rounded" src="/images/user/05.jpg" alt="" />
-                          </div>
-                          <div className="ms-3">
-                            <h6 className="mb-0 ">Lorem Ipsum generators</h6>
-                            <small className="float-left font-size-12">5 Dec</small>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </li>
-
+              
+             
               <li className="nav-item dropdown">
                 <a href="/Profile" className="   d-flex align-items-center dropdown-toggle" id="drop-down-arrow" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <img src="/images/user/1.jpg" className="img-fluid rounded-circle me-3" alt="user" />
                   <div className="caption">
                     <h6 className="mb-0 line-height">Bni Cyst</h6>
                   </div>
-                
                 </a>
                 <div className="sub-drop dropdown-menu caption-menu" aria-labelledby="drop-down-arrow">
                   <div className="card shadow-none m-0">
